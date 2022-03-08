6b55d8b99eed334afa084841f1377d7fdc2efcce
LuizBlading <luizfelipe1342@yahoo.com.br>
2022-03-08 02:35:19 -0300

    Create App and Add Menu

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { MenuComponent } from '../components/menu/menu.component';
 
 @NgModule({
   imports: [
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, MenuComponent]
 })
 export class HomePageModule {}
